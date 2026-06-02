0379d8c8c4a482ec687e1043f53eaa0c5e79c83c
Marcos <msa.felipesousa@gmail.com>
2026-06-02 11:13:52 -0300

    feat: implementar a validação de dados

--- src/components/NewFactForm.tsx
+++ src/components/NewFactForm.tsx
@@ -1,6 +1,8 @@
 import type { RefObject } from "react"
 import { useState } from "react"
 import { CATEGORIES } from "../constants"
+import { factSchema } from "./schemas"
+import { success } from "zod"
 
 interface NewFactFormProps {
     inputRef: RefObject<HTMLInputElement>
@@ -11,16 +13,31 @@ export default function NewFactForm ({ inputRef }: NewFactFormProps ) {
     const [text, setText] =useState<string>('')
     const [source, setSource] =useState<string>('')
     const [category, setCategory] =useState<string>('')
+    const [error, setError] = useState<Record<string, string[]>>({})
 
     const charRemaining = 200 - text.length
 
     function handleSubmit ( event: React.SubmitEvent<HTMLFormElement> ) {
         event.preventDefault()
 
+        const result = factSchema.safeParse({
+            text,
+            source,
+            category
+        })
         
-        console.log({ text, source, category })
+        if (!result.success) {
+            setError(result.error.flatten().fieldErrors)
+            return;
+        }
+
+        setError({})
+        console.log(result.data)
         
     }
+    // function handlerError (errorData) {
+    //     return (error.errorData && error.source[0] && <span>{error.source[0]}</span>)
+    // }
 
 
     const categoryOptions = CATEGORIES.map((category) => (
@@ -41,12 +58,14 @@ export default function NewFactForm ({ inputRef }: NewFactFormProps ) {
                     maxLength={200}
                     />
                 <span>{charRemaining}</span>
+                {error.text && error.text[0] && <span>{error.text[0]}</span>}
                 <input 
                     type="text"
                     placeholder="URL da fonte(https://...)"
                     value={source}
                     onChange={event => setSource(event.target.value)}
                 />
+                {error.source && error.source[0] && <span>{error.source[0]}</span>}
                 <select
                     value={category}
                     onChange={event => setCategory(event.target.value)}
@@ -54,6 +73,7 @@ export default function NewFactForm ({ inputRef }: NewFactFormProps ) {
                     <option value="">Selecione uma categoria</option>
                     {categoryOptions}
                 </select>
+                {error.category && error.category[0] && <span>{error.category[0]}</span>}
                 <button type="submit">publicar</button>
             </form>
         </>
