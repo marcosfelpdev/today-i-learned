3c1f57e3704335e883dab82984e24b5db92c238b
Marcos <msa.felipesousa@gmail.com>
2026-06-09 09:00:20 -0300

    refactory: aplica lógica de categoria em português

--- src/components/FactItem.tsx
+++ src/components/FactItem.tsx
@@ -1,16 +1,21 @@
 import type { Fact } from "../types";
+import { CATEGORIES } from "../constants";
 
 interface FactItemProps {
     fact: Fact
 }
 
 export default function FactItem({ fact }: FactItemProps) { 
+    const category = CATEGORIES.find((category) => {
+        return fact.category === category.value;
+    })
+
     return (
         <>
             <li>
                 <p>{fact.text}</p>
-                <a href={fact.source} target="_blank" rel="noreferrer">Fonte</a>
-                <span>{fact.category}</span>
+                <a href={fact.source} target="_blank" rel="noreferrer">(Fonte)</a>
+                <span>{ category?.label}</span>
             </li>
         </>
     )
